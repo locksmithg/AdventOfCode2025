ed208ccc29bb4f9ad48ea26a02f85a2c0a3bd458
Gabriel Smith <gabriel.smith@tealdrones.com>
2025-12-02 09:39:49 -0700

    d2p2

--- day2.py
+++ day2.py
@@ -13,7 +13,29 @@ def day2_part1(input):
                 # print(i)
     print(sumInvalidIds)
 
+def day2_part2(input):
+    sumInvalidIds = 0
+    for ids in input.split(','):# for each id range
+        lower = ids.strip().split('-')[0]
+        upper = ids.strip().split('-')[1]
+        for i in range(int(lower), int(upper)+1): # for each id in range
+            iChars = str(i) # convert to string
+            half = int(len(iChars)/2) # 
+            for j in range(1, half+1):
+                if len(iChars)%j != 0:
+                    continue
+                sequence = iChars[0:j]
+                valid = False
+                for k in range(0, len(iChars), j):
+                    if iChars[k:k+len(sequence)] != sequence:
+                        valid = True
+                if not valid:
+                    # print(i)
+                    sumInvalidIds += i
+                    break
+    print(sumInvalidIds)
 
 if __name__ == "__main__":
     data = open("input2.txt").read()
-    day2_part1(data)
+    #day2_part1(data)
+    day2_part2(data)
